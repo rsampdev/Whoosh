6f180f30b8b77c7e5d5db7c9e9c4021a421e953a
rsampdev <rsampdev@gmail.com>
2016-08-30 17:49:10 -0400

    Initial Commit

---
+++ Whoosh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Whoosh
+//
+//  Created by Rodney Sampson on 8/30/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Whoosh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Whoosh
+//
+//  Created by Rodney Sampson on 8/30/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
